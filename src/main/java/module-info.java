94530584eff58de8fd9c7278291979a0c1b650c1
WidgetMidget <67565373+WidgetMidget@users.noreply.github.com>
2024-02-08 19:52:49 +0100

    feat: Add main-menu and some code to let you actually use it.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module com.frigvid.jman {
     requires javafx.controls;
     requires javafx.fxml;
-
-
+    
     opens com.frigvid.jman to javafx.fxml;
     exports com.frigvid.jman;
+    
+    exports com.frigvid.jman.view;
+    opens com.frigvid.jman.view;
 }
