10fb272b26dae94814a70d6f9dd712eacb425c5d
frigvid <67565373+frigvid@users.noreply.github.com>
2024-02-08 19:52:49 +0100

    feat: Add main-menu and some code to let you actually use it.

--- src/main/java/com/frigvid/jman/Main.java
+++ src/main/java/com/frigvid/jman/Main.java
@@ -1,17 +1,21 @@
 package com.frigvid.jman;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
+    private static final int DEFAULT_WIDTH = 800;
+    private static final int DEFAULT_HEIGHT = 500;
+    
     @Override
     public void start(Stage stage) throws IOException {
-        BorderPane root = new BorderPane();
-        Scene scene = new Scene(root, 320, 240);
+        //BorderPane root = new BorderPane();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/frigvid/jman/view/main-menu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), DEFAULT_WIDTH, DEFAULT_HEIGHT);
         stage.setTitle("J-Man!");
         stage.setScene(scene);
         stage.show();
