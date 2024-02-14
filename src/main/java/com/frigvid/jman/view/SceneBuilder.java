9f30afae05737c060b35d0088286d240d786ee98
frigvid <67565373+frigvid@users.noreply.github.com>
2024-02-14 21:32:38 +0100

    feat: Add FXML handling to scene builder.

--- src/main/java/com/frigvid/jman/view/SceneBuilder.java
+++ src/main/java/com/frigvid/jman/view/SceneBuilder.java
@@ -1,15 +1,17 @@
 package com.frigvid.jman.view;
 
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 /**
- *
  * @author frigvid
+ * @version 0.1
  * @created 2024-02-14
  * @since 0.1
- * @version 0.1
  */
 public class SceneBuilder
 {
@@ -18,6 +20,7 @@ public class SceneBuilder
 	private String title = "J-Man!";
 	private int width = 250;
 	private int height = 250;
+	private String fxmlPath;
 	
 	public SceneBuilder setStage(Stage stage)
 	{
@@ -49,15 +52,41 @@ public class SceneBuilder
 		return this;
 	}
 	
+	public SceneBuilder setFxml(String fxmlPath)
+	{
+		this.fxmlPath = fxmlPath;
+		return this;
+	}
+	
 	public void build()
 	{
+		// Catch generic errors first.
 		if (stage == null)
 		{
 			throw new IllegalStateException("Primary stage not set!");
 		}
-		else if (root == null)
+		else if (root == null && fxmlPath == null)
 		{
-			throw new IllegalStateException("Root element not set!");
+			throw new IllegalStateException("Root element or FXML path not set!");
+		}
+		
+		/* Check if the root is an FXML file.
+		 *
+		 * NOTE: Ignore the warning that fxmlPath is always true.
+		 */
+		if (root == null && fxmlPath != null)
+		{
+			try
+			{
+				FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
+				root = fxmlLoader.load();
+			}
+			catch (IOException e)
+			{
+				// This is kind of ugly, but it works.
+				System.err.println("Failed to load FXML file: " + fxmlPath);
+				return; // Guarantee exit.
+			}
 		}
 		
 		Scene scene = new Scene(root, width, height);
