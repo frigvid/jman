b5f5212348226dcc106591b0e78f400e684604e4
frigvid <67565373+frigvid@users.noreply.github.com>
2024-02-15 12:49:33 +0100

    feat: Create Constants class.

---
+++ src/main/java/com/frigvid/jman/Constants.java
@@ -0,0 +1,12 @@
+package com.frigvid.jman;
+
+/**
+ * A class for storing constants used in the application.
+ */
+public final class Constants
+{
+	private Constants()
+	{
+		// Restricted instantiation.
+	}
+}
