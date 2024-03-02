576962af1b9fbae508d0cf84d1215536f8f273ed
frigvid <67565373+frigvid@users.noreply.github.com>
2024-03-02 23:10:18 +0100

    chore: Enforce editor config onto module-info.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,12 @@
 module com.frigvid.jman {
-    requires javafx.controls;
-    requires javafx.fxml;
-    
-    opens com.frigvid.jman to javafx.fxml;
-    exports com.frigvid.jman;
-    
-    exports com.frigvid.jman.view;
-    opens com.frigvid.jman.view;
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens com.frigvid.jman to javafx.fxml;
+	exports com.frigvid.jman;
+	
+	exports com.frigvid.jman.view;
+	opens com.frigvid.jman.view;
 	exports com.frigvid.jman.view.state;
 	opens com.frigvid.jman.view.state;
 }
